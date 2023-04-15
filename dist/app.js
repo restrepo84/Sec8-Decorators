b15279965efb051612f99c2bd56d6f0c0cce2a02
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-15 01:31:17 -0600

    Building more useful decorators

--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,16 @@ function Logger(logString) {
         console.log(constructor);
     };
 }
+function WithTemplate(template, hookId) {
+    return function (constructor) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1").textContent = p.name;
+        }
+    };
+}
 let Person = class Person {
     constructor() {
         this.name = "Steve";
@@ -18,7 +28,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger("LOGGING - PERSON")
+    WithTemplate("<h1>My Person Object</h1>", "app")
 ], Person);
 const pers = new Person();
 console.log(pers);
